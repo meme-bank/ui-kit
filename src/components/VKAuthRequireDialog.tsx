132e4a90c35b18f058173d9d624d914ad09e6237
Urtjom-Aljanow <mc2017artemklochkcov435@yandex.ru>
2023-08-24 18:30:09 +0300

    Header fix + VKAutDialog

---
+++ src/components/VKAuthRequireDialog.tsx
@@ -0,0 +1,30 @@
+import * as React from "react"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog"
+import { VKAuthButton } from "./ui/vk-auth-button"
+import { VKIcon } from "./ui/vk-icon"
+
+export const VKAuthRequireDialog: React.FC<{ connect?: () => void }> = ({ connect }) => {
+    return (
+        <Dialog open>
+            <DialogContent autoClose>
+                <DialogHeader>
+                    <DialogTitle>
+                        <span className="flex items-center justify-center sm:justify-start gap-2">
+                            <VKIcon className="h-4 w-4" />
+                            Подключение VK ID
+                        </span>
+                    </DialogTitle>
+                    <DialogDescription className="space-y-2">
+                        <p>Для использования НБМ, требуется подключить Ваш аккаунт VK ID к Вашей учётной записи НБМ<sup className="text-muted-foreground">Sanctum</sup>. Это требуется для проверки подлинности учётной записи, а также для осуществления привязки государств и организаций к группам VK.</p>
+                        <p>Также регистрация новых учётных записей доступна только через предварительную авторизацию через VK ID</p>
+                        <p>Для Подключения VK ID к Вашей учётной записи нажмите на кнопку ниже</p>
+                        <p>Спасибо, что используете НБМ ;)</p>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <VKAuthButton onClick={connect} />
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
