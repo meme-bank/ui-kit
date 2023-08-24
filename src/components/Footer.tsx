68ec63f0bfad7d32fd048db853c3df81dafa6aa6
Urtjom-Aljanow <mc2017artemklochkcov435@yandex.ru>
2023-08-24 15:50:32 +0300

    Footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,11 @@ export const Footer: React.FC = () => {
     return (
         <footer className="container p-2 mx-auto flex flex-col gap-4">
             <div className="text-muted-foreground flex-row gap-2 flex-wrap">
-                <Button asChild variant={"link"} className="text-muted-foreground hover:text-vk-azure"><a href="https://vk.com/club206737619">Мы ВКонтакте</a></Button>
-                <Button asChild variant={"link"} className="text-muted-foreground hover:text-primary-foreground"><a href="https://www.npmjs.com/org/meduza-bank">Мы в NPM</a></Button>
-                <Button asChild variant={"link"} className="text-muted-foreground hover:text-primary-foreground"><a href="https://github.com/meme-bank">Мы в Github</a></Button>
+                <Button asChild variant={"link"} className="text-muted-foreground hover:text-vk-azure"><a target="_blank" href="https://vk.com/club206737619">Мы ВКонтакте</a></Button>
+                <Button asChild variant={"link"} className="text-muted-foreground hover:text-primary"><a target="_blank" href="https://www.npmjs.com/org/meduza-bank">Мы в NPM</a></Button>
+                <Button asChild variant={"link"} className="text-muted-foreground hover:text-primary"><a target="_blank" href="https://github.com/meme-bank">Мы в Github</a></Button>
             </div>
-            <Separator />
+            <Separator className="w-full" />
             <p className="text-sm text-muted-foreground">© Народный Банк Мемов, 2020-{new Date().getFullYear()}, Права не защищены, но пиздить не хорошо!</p>
         </footer>
     )
