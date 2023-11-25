1ab58a8a404df35d58f529e3f738dd3bd5fd200d
Urtjom-Aljanow <mc2017artemklochkcov435@yandex.ru>
2023-11-25 17:11:01 +0300

    provide div props to line-block element

--- src/components/ui/line-block.tsx
+++ src/components/ui/line-block.tsx
@@ -1,9 +1,9 @@
 import { cn } from "@/lib/utils";
 import * as React from "react";
 
-export const LineBlock: React.FC<React.PropsWithChildren<{ className?: string }>> = ({ children, className }) => {
+export const LineBlock: React.FC<React.PropsWithChildren<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>>> = ({ children, className, ...props }) => {
     return (
-        <div className={cn("rounded-md border p-2", className)}>
+        <div className={cn("rounded-md border p-2", className)} {...props}>
             {children}
         </div>
     )
