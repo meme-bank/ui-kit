7928be466b1825b4518bcfb25f3db001f71c4a63
Urtjom-Aljanow <mc2017artemklochkcov435@yandex.ru>
2024-07-09 22:26:41 +0300

    add noncontrol input field form

--- src/components/InputFormField.tsx
+++ src/components/InputFormField.tsx
@@ -3,6 +3,8 @@ import { Skeleton } from "@ui-components/skeleton";
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@ui-components/form";
 import { Input, InputProps } from "@ui-components/input";
 import { LucideIcon } from "lucide-react";
+import { Label } from "./ui/label";
+import { cn } from "@/lib/utils";
 
 export interface InputFormFieldProps extends InputProps {
     label?: string;
@@ -11,9 +13,11 @@ export interface InputFormFieldProps extends InputProps {
     placeholder?: string;
     description?: string;
     LabelIcon?: LucideIcon;
+    nonControl?: boolean;
+    error?: string;
 }
 
-export const InputFormField: React.FC<InputFormFieldProps> = ({ label, name, skeletonLoad, placeholder, description, LabelIcon, ...props }) => {
+export const InputFormField: React.FC<InputFormFieldProps> = ({ label, name, skeletonLoad, placeholder, description, nonControl, error, LabelIcon, ...props }) => {
     if (skeletonLoad) {
         return (
             <div className="ms-space-y-2">
@@ -30,6 +34,28 @@ export const InputFormField: React.FC<InputFormFieldProps> = ({ label, name, ske
             </div>
         )
     }
+
+    if (nonControl)
+        return (
+            <div className="ms-space-y-2">
+                <Label className={error && "ms-text-destructive"}>
+                    <span className="ms-flex ms-items-center ms-gap-2">
+                        {LabelIcon && <LabelIcon className="h-4 w-4" />}
+                        {label}
+                    </span>
+                </Label>
+                <Input {...props} placeholder={placeholder} />
+                {description && <p className="ms-text-sm ms-text-muted-foreground">
+                    {description}
+                </p>}
+                {error && <p
+                    className={cn("ms-text-sm ms-font-medium ms-text-destructive")}
+                >
+                    {error}
+                </p>}
+            </div>
+        )
+
     return (
         <FormField name={name} render={({ field }) => (
             <FormItem>
