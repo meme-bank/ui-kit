0ff87e5e4f8b41fe15b0e4740faefc9d290be2d6
Urtyom <mc2017artemklochkcov435@yandex.ru>
2024-08-20 00:22:39 +0300

    iff rhf fix

--- src/components/InputFormField.tsx
+++ src/components/InputFormField.tsx
@@ -1,78 +1,99 @@
 import { cn } from "@/lib/utils";
-import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@ui-components/form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@ui-components/form";
 import { Input, InputProps } from "@ui-components/input";
 import { Skeleton } from "@ui-components/skeleton";
 import { LucideIcon } from "lucide-react";
 import * as React from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
 import { Label } from "./ui/label";
 
 export interface InputFormFieldProps extends InputProps {
-    label?: string;
-    name: string;
-    skeletonLoad?: boolean;
-    placeholder?: string;
-    description?: string;
-    LabelIcon?: LucideIcon;
-    rhmControl?: boolean;
-    error?: string;
+  label?: string;
+  name: string;
+  skeletonLoad?: boolean;
+  description?: string;
+  LabelIcon?: LucideIcon;
+  rhfControl?: boolean | UseFormRegisterReturn;
+  error?: string;
 }
 
-export const InputFormField: React.FC<InputFormFieldProps> = ({ label, name, skeletonLoad, placeholder, description, rhmControl, error, LabelIcon, ...props }) => {
-    if (skeletonLoad) {
-        return (
-            <div className="ms-space-y-2">
-                <Skeleton className="ms-inline-block ms-leading-none ms-text-sm ms-font-medium ms-rounded-md">
-                    <span className="ms-invisible ms-inline-flex ms-items-center ms-gap-2">
-                        {LabelIcon && <LabelIcon className="h-4 w-4" />}
-                        {label}
-                    </span>
-                </Skeleton>
-                <Skeleton className="ms-h-10 ms-w-full ms-rounded-md ms-px-3 ms-py-2 ms-text-sm" />
-                <Skeleton className="ms-inline-block ms-rounded-md">
-                    <span className="ms-invisible ms-text-sm ms-text-muted-foreground">{description}</span>
-                </Skeleton>
-            </div>
-        )
-    }
-
-    if (!rhmControl)
-        return (
-            <div className="ms-space-y-2">
-                <Label className={error && "ms-text-destructive"}>
-                    <span className="ms-flex ms-items-center ms-gap-2">
-                        {LabelIcon && <LabelIcon className="ms-h-4 ms-w-4" />}
-                        {label}
-                    </span>
-                </Label>
-                <Input {...props} name={name} placeholder={placeholder} />
-                {description && <p className="ms-text-sm ms-text-muted-foreground">
-                    {description}
-                </p>}
-                {error && <p
-                    className={cn("ms-text-sm ms-font-medium ms-text-destructive")}
-                >
-                    {error}
-                </p>}
-            </div>
-        )
-
+export const InputFormField: React.FC<InputFormFieldProps> = ({
+  label,
+  name,
+  skeletonLoad,
+  description,
+  rhfControl,
+  error,
+  LabelIcon,
+  ...props
+}) => {
+  if (skeletonLoad) {
     return (
-        <FormField name={name} render={({ field }) => (
-            <FormItem>
-                {label &&
-                    <FormLabel>
-                        <span className="ms-flex ms-items-center ms-gap-2">
-                            {LabelIcon && <LabelIcon className="ms-h-4 ms-w-4" />}
-                            {label}
-                        </span>
-                    </FormLabel>
-                }
-                <FormControl>
-                    <Input {...props} {...field} placeholder={placeholder} />
-                </FormControl>
-                {description && <FormDescription>{description}</FormDescription>}
-                <FormMessage />
-            </FormItem>
-        )} />
-    )
-}
+      <div className="ms-space-y-2">
+        <Skeleton className="ms-inline-block ms-leading-none ms-text-sm ms-font-medium ms-rounded-md">
+          <span className="ms-invisible ms-inline-flex ms-items-center ms-gap-2">
+            {LabelIcon && <LabelIcon className="h-4 w-4" />}
+            {label}
+          </span>
+        </Skeleton>
+        <Skeleton className="ms-h-10 ms-w-full ms-rounded-md ms-px-3 ms-py-2 ms-text-sm" />
+        <Skeleton className="ms-inline-block ms-rounded-md">
+          <span className="ms-invisible ms-text-sm ms-text-muted-foreground">
+            {description}
+          </span>
+        </Skeleton>
+      </div>
+    );
+  }
+
+  if (!rhfControl || typeof rhfControl === "object")
+    return (
+      <div className="ms-space-y-2">
+        <Label className={error && "ms-text-destructive"}>
+          <span className="ms-flex ms-items-center ms-gap-2">
+            {LabelIcon && <LabelIcon className="ms-h-4 ms-w-4" />}
+            {label}
+          </span>
+        </Label>
+        <Input name={name} {...rhfControl} {...props} />
+        {description && (
+          <p className="ms-text-sm ms-text-muted-foreground">{description}</p>
+        )}
+        {error && (
+          <p className={cn("ms-text-sm ms-font-medium ms-text-destructive")}>
+            {error}
+          </p>
+        )}
+      </div>
+    );
+
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && (
+            <FormLabel>
+              <span className="ms-flex ms-items-center ms-gap-2">
+                {LabelIcon && <LabelIcon className="ms-h-4 ms-w-4" />}
+                {label}
+              </span>
+            </FormLabel>
+          )}
+          <FormControl>
+            <Input {...props} {...field} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,15 +1,14 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> { }
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, ...props }, ref) => {
     return (
       <input
-        type={type}
         className={cn(
           "ms-flex ms-h-10 ms-w-full ms-rounded-md ms-border ms-border-input ms-bg-background ms-px-3 ms-py-2 ms-text-sm ms-ring-offset-background file:ms-border-0 file:ms-bg-transparent file:ms-text-sm file:ms-font-medium placeholder:ms-text-muted-foreground focus-visible:ms-outline-none focus-visible:ms-ring-2 focus-visible:ms-ring-ring focus-visible:ms-ring-offset-2 disabled:ms-cursor-not-allowed disabled:ms-opacity-50",
           className
@@ -17,9 +16,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
