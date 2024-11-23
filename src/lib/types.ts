e71aa11bf0924ad16e4dafdc13dfb777eca8f999
Urtyom <mc2017artemklochkcov435@yandex.ru>
2024-11-23 19:39:21 +0300

    Iconable type fix

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,4 +2,6 @@ import { ExoticComponent, FunctionComponent } from "react";
 
 export type Iconable =
   | FunctionComponent<{ className?: string | null }>
-  | ExoticComponent<{ className?: string | null }>;
+  | ExoticComponent<{ className?: string | null }>
+  | FunctionComponent<{ className?: string }>
+  | ExoticComponent<{ className?: string }>;
