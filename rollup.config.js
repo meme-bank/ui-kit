c192e65fafe00108add08918174aa8e4a281e1e9
Urtyom <mc2017artemklochkcov435@yandex.ru>
2024-12-08 01:50:51 +0300

    update vaul

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import { dts } from "rollup-plugin-dts";
 import alias from "@rollup/plugin-alias";
+import commonjs from "@rollup/plugin-commonjs";
+import image from "@rollup/plugin-image";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 import path from "path";
-import image from '@rollup/plugin-image';
-import terser from '@rollup/plugin-terser';
-import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
+import { dts } from "rollup-plugin-dts";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import useClient from "rollup-plugin-use-client";
 
 const packageJson = require("./package.json");
 
@@ -19,7 +20,7 @@ export default [
         file: packageJson.main,
         format: "cjs",
         sourcemap: true,
-        interop: "compat"
+        interop: "compat",
       },
       {
         file: packageJson.module,
@@ -32,10 +33,16 @@ export default [
       alias({
         entries: [
           { find: "@", replacement: path.resolve(__dirname, "./src") },
-          { find: "@ui-components", replacement: path.resolve(__dirname, "./src/components/ui") },
-          { find: "@components", replacement: path.resolve(__dirname, "./src/components") },
+          {
+            find: "@ui-components",
+            replacement: path.resolve(__dirname, "./src/components/ui"),
+          },
+          {
+            find: "@components",
+            replacement: path.resolve(__dirname, "./src/components"),
+          },
           { find: "@lib", replacement: path.resolve(__dirname, "./src/lib") },
-        ]
+        ],
       }),
       external(),
       resolve(),
@@ -43,7 +50,8 @@ export default [
       image(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
-      terser()
+      useClient(),
+      terser(),
     ],
   },
   {
@@ -54,12 +62,18 @@ export default [
       alias({
         entries: [
           { find: "@", replacement: path.resolve("./dist/esm/types/") },
-          { find: "@ui-components", replacement: path.resolve("./dist/esm/types/components/ui") },
-          { find: "@components", replacement: path.resolve("./dist/esm/types/components") },
+          {
+            find: "@ui-components",
+            replacement: path.resolve("./dist/esm/types/components/ui"),
+          },
+          {
+            find: "@components",
+            replacement: path.resolve("./dist/esm/types/components"),
+          },
           { find: "@lib", replacement: path.resolve("./dist/esm/types/lib") },
-        ]
+        ],
       }),
-      dts()
+      dts(),
     ],
   },
 ];
