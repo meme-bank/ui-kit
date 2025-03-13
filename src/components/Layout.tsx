cd85c0945c9d0a92015e78233de0ade81d80542a
Urtyom-Alyanov <mc2017artemklochkov435@yandex.ru>
2025-03-13 21:43:03 +0300

    add layout navigate state

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 import { Footer } from "./Footer";
 import { Separator } from "./ui/separator";
 
-export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+export const Layout: React.FC<React.PropsWithChildren<{ isNavigate?: boolean }>> = ({ children, isNavigate }) => {
   return (
     <div className="ms:flex ms:flex-col ms:gap-4">
       <div className="ms:h-14" />
-      <div className="ms:p-2 ms:mx-auto ms:container">{children}</div>
+      <div className="ms:p-2 ms:mx-auto ms:container ms:group/page" data-navigate={isNavigate}>{children}</div>
       <Separator className="ms:mx-auto" />
       <Footer />
     </div>
